838d9227ef62f3e031804ab8b28c49535e26fe84
EduardaCosta-hub <eduarda.schuler.costa@gmail.com>
2023-03-01 11:12:55 -0300

    updates

--- extrator_url.py
+++ extrator_url.py
@@ -1,29 +1,23 @@
 import re
 class ExtratorURL:
-    def __init__(self, purl):
-        self.__url = purl.strip
-        print(purl)
-        print(self.__url)
-
+    def __init__(self, pUrl:str):
+        self.__url = pUrl.strip()
+        
+    #methods
     def valida_url(self):
         url = self.__url
         print(url)
         if not url:  
             raise ValueError("A URL está vazia")
         
-        padrao_url = re.compile('(http(s)?://)?(www.)?w3schools.com(.br)?/python')
+        padrao_url = re.compile('(http(s)?://)?(www.)?google.com(.br)?/search')
         match = padrao_url.match(url)
         if not match:
             raise ValueError("A URL não é válida.")
         print("URL validada com sucesso!")
-
-    def get_url_base(self):
-        """Retorna a base da url."""
-        ...
-
-    def get_url_parametros(self):
-        """Retorna os parâmetros da url."""
-        ...
-
-    def get_valor_parametro(self, parametro_busca):
-        """Retorna o valor do parametro `parametro_busca`."""
+    
+    #properties
+    
+    @property    
+    def url(self):
+        return self.__url
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from extrator_url import ExtratorURL
-url = 'https://www.w3schools.com/python?'
-separador = url.find("?")
+url = 'https://www.google.com/search?q=gerador+de+cpf'
+url_extraida = ExtratorURL(url)
+print(url_extraida.url)
+url_extraida.valida_url()
+'''separador = url.find("?")
 url_base = url[:separador]
 url_parametros = url[separador+1:]
 print(url)
 print(url_base)
-print(url_parametros)
+print(url_parametros)'''
