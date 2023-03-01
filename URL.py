c9353c57a1c76e4b878d0e3ee5b8c9173d2d4707
EduardaCosta-hub <eduarda.schuler.costa@gmail.com>
2023-03-01 12:02:23 -0300

    updates

--- URL.py
+++ URL.py
@@ -8,21 +8,35 @@ class URL:
         if type(pUrl) == str:
             return pUrl.strip()
         else:  
-            return ValueError("É necessário informar um valor do tipo String(texto)")
+            raise ValueError("É necessário informar um valor do tipo String(texto)")
+
     def valida_url(self):
-        url = self.__url
-        print(url)
-        if not url:  
+        if not self.__url:  
             raise ValueError("A URL está vazia")
              
         padrao_url = re.compile('(http(s)?://)?(www.)?google.com(.br)?/search')
-        match = padrao_url.match(url)
+        match = padrao_url.match(self.__url)
         if not match:
             raise ValueError("A URL não é válida.")
         print("URL validada com sucesso!")
-    
+        return True
+                
     #properties
     
     @property    
-    def url(self):
+    def url_completa(self):
         return self.__url
+    
+    @property
+    def url_base(self):
+        if self.valida_url():
+            separador = self.__url.find("?")
+            url_base = self.__url[:separador]
+            return "URL base: " + url_base
+    
+    @property
+    def url_parametros(self):
+        if self.valida_url():
+            separador = self.__url.find("?")
+            url_parametros = self.__url[(separador+1):]
+            return "URL parâmetros: " + url_parametros
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from URL import URL
-url_numerica = URL(123)
-'''separador = url.find("?")
-url_base = url[:separador]
-url_parametros = url[separador+1:]
-print(url)
-print(url_base)
-print(url_parametros)
 url_google = URL('https://www.google.com/search?q=gerador+de+cpf')
-url_vazia = URL("   ")'''
+print(url_google.url_base)
+print(url_google.url_parametros)
+
+#tests
+
+##url_vazia = URL("   ")
+##url_vazia.valida_url()
+##url_numerica = URL(123)
