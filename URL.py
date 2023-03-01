164d63bf34ec64af791563a3e98ba18a19557096
EduardaCosta-hub <eduarda.schuler.costa@gmail.com>
2023-03-01 11:25:24 -0300

    updates

--- URL.py
+++ URL.py
@@ -1,15 +1,20 @@
 import re
-class ExtratorURL:
-    def __init__(self, pUrl:str):
-        self.__url = pUrl.strip()
+class URL:
+    def __init__(self, pUrl):
+        self.__url = self.higieniza_url(pUrl)
         
     #methods
+    def higieniza_url(self, pUrl):
+        if type(pUrl) == str:
+            return pUrl.strip()
+        else:  
+            return ValueError("É necessário informar um valor do tipo String(texto)")
     def valida_url(self):
         url = self.__url
         print(url)
         if not url:  
             raise ValueError("A URL está vazia")
-        
+             
         padrao_url = re.compile('(http(s)?://)?(www.)?google.com(.br)?/search')
         match = padrao_url.match(url)
         if not match:
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from extrator_url import ExtratorURL
-url = 'https://www.google.com/search?q=gerador+de+cpf'
-url_extraida = ExtratorURL(url)
-print(url_extraida.url)
-url_extraida.valida_url()
+from URL import URL
+url_numerica = URL(123)
 '''separador = url.find("?")
 url_base = url[:separador]
 url_parametros = url[separador+1:]
 print(url)
 print(url_base)
-print(url_parametros)'''
+print(url_parametros)
+url_google = URL('https://www.google.com/search?q=gerador+de+cpf')
+url_vazia = URL("   ")'''
